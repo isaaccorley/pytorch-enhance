df1aa31f7a811bb5aec08422e1c87f0bf4e48414
IsaacCorley <isaac.a.corley@gmail.com>
2020-06-30 12:17:15 -0500

    changed version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 url = "https://github.com/IsaacCorley/pytorch_enhance"
 
 with open("requirements.txt", "r") as f:
--- torch_enhance/__init__.py
+++ torch_enhance/__init__.py
@@ -3,7 +3,7 @@ import torch_enhance.datasets
 import torch_enhance.metrics
 import torch_enhance.losses
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 
 __all__ = [
     "torch_enhance",
