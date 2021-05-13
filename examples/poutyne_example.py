e4c518574a25002368245a0b0ff2d2f321ca0aa6
IsaacCorley <isaac.a.corley@gmail.com>
2021-05-12 19:40:53 -0500

    updated examples

--- examples/poutyne_example.py
+++ examples/poutyne_example.py
@@ -9,12 +9,13 @@ from torch_enhance import metrics
 
 
 scale_factor = 2
-train_dataset = BSDS300(scale_factor=scale_factor, data_dir="../.data/")
-val_dataset = Set14(scale_factor=scale_factor, data_dir="../.data/")
+train_dataset = BSDS300(scale_factor=scale_factor)
+val_dataset = Set14(scale_factor=scale_factor)
 train_dataloader = DataLoader(train_dataset, batch_size=8)
 val_dataloader = DataLoader(val_dataset, batch_size=2)
 
-pytorch_network = SRCNN(scale_factor)
+channels = 3 if train_dataset.color_space == "RGB" else 1
+pytorch_network = SRCNN(scale_factor, channels)
 
 model = Model(
     pytorch_network,
