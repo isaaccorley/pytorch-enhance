c341a19a3480433eb41ac83be25ac557f1f97219
isaac <22203655+isaaccorley@users.noreply.github.com>
2021-05-13 09:09:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.1.4"
+__version__ = "0.1.6"
 url = "https://github.com/IsaacCorley/pytorch-enhance"
 
 with open("requirements.txt", "r") as f:
