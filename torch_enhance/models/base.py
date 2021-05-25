0a2fadf6425d0ec1ffa275343a0a90861be32526
IsaacCorley <isaac.a.corley@gmail.com>
2021-05-25 16:15:16 -0500

    update enhance method

--- torch_enhance/models/base.py
+++ torch_enhance/models/base.py
@@ -72,10 +72,13 @@ class BaseModel(nn.Module):
             Super-Resolved image as tensor
 
         """
+        if x.ndim == 3:
+            x = x.unsqueeze(0)
+
         x = self.forward(x)
         x *= 255.0
         x = x.clamp(0, 255)
         x = x.to(torch.uint8)
-        x = x.squeeze()
-        x = x.transpose((1, 2, 0))
+        x = x.squeeze(0)
+        x = x.permute(1, 2, 0)
         return x
